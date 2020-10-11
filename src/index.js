5059bde96c95f287a7143ad9e8c54e25c68aea2f
FireYourGuns <chazovby@gmail.com>
2020-10-11 12:53:40 +0300

    feat: implement all requires of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  return true;
+  var str_2 = "";
+  var New_array = [];
+  for (var i = 0; i < bracketsConfig.length; i++) {
+    var X = bracketsConfig[i];
+    var l = X.length;
+    if (X = Array) { //только такое равенство
+          str_2 = "";
+          for (var j = 0; j < l; j++) {
+            str_2 += bracketsConfig[i][j];
+          }
+          New_array.push(str_2);
+    } else {
+          str_2 += bracketsConfig[i];
+        }
+    
+  }
+    if (New_array == []) {  // такое == или такое ===
+      New_array.push(str_2)
+    }
+var T = str.length/2;
+    for (j = 0; j < T; j++) {
+      for (i = 0; i < New_array.length; i++) {
+      str = str.replace(New_array[i], '');
+    }
+    }
+    
+    
+  return  str === "" ? true : false
 }
--- src/index2.js
+++ src/index2.js
@@ -1,83 +1,36 @@
-const MORSE_TABLE = {
-  '.-':     'a',
-  '-...':   'b',
-  '-.-.':   'c',
-  '-..':    'd',
-  '.':      'e',
-  '..-.':   'f',
-  '--.':    'g',
-  '....':   'h',
-  '..':     'i',
-  '.---':   'j',
-  '-.-':    'k',
-  '.-..':   'l',
-  '--':     'm',
-  '-.':     'n',
-  '---':    'o',
-  '.--.':   'p',
-  '--.-':   'q',
-  '.-.':    'r',
-  '...':    's',
-  '-':      't',
-  '..-':    'u',
-  '...-':   'v',
-  '.--':    'w',
-  '-..-':   'x',
-  '-.--':   'y',
-  '--..':   'z',
-  '.----':  '1',
-  '..---':  '2',
-  '...--':  '3',
-  '....-':  '4',
-  '.....':  '5',
-  '-....':  '6',
-  '--...':  '7',
-  '---..':  '8',
-  '----.':  '9',
-  '-----':  '0',
-};
-
-function decode(expr) {
-  var Mas = expr.split("");
+function check(str, bracketsConfig) {
+  /*if (!(str.includes('()') || str.includes('[]') || str.includes('{}') || str.includes('||'))) {
+    return false
+  } */
+  var str_2 = "";
   var New_array = [];
-  var x = Mas.length/10;
-  var Code = '';
-  var Frase = '';
-  for (var i = 1; i <= x; i++) {
-      New_array.push(Mas.splice(0,10).join(""));
-  }
-
-  for (i = 0; i < New_array.length; i++) {
-    var Z = New_array[i].split('');
-      if (New_array[i] == '**********') {
-        Frase += ' ';
-      } else {
-                  Code = '';
-                  for (var j=1; j<6; j++) {
-                      var Twix = Z.splice(0,2).join('');
-                      if (Twix == 10) {
-                      Code += ".";
-                      }
-                      if (Twix == 11) {
-                      Code += "-";
-                      }
-                  }
-              Frase += MORSE_TABLE[Code];
+  for (var i = 0; i < bracketsConfig.length; i++) {
+    var X = bracketsConfig[i];
+    var l = X.length;
+    if (X = Array) { //только такое равенство
+          str_2 = "";
+          for (var j = 0; j < l; j++) {
+            str_2 += bracketsConfig[i][j];
           }
-      }
+          New_array.push(str_2);
+    } else {
+          str_2 += bracketsConfig[i];
+        }
+    
+  }
+    if (New_array == []) {  // такое == или такое ===
+      New_array.push(str_2)
+    }
+var T = str.length/2;
+    for (j = 0; j < T; j++) {
+      for (i = 0; i < New_array.length; i++) {
+      str = str.replace(New_array[i], '');
+    }
+    }
+    
+    
+  return  str === "" ? true : false
 
-  return Frase;
 }
- 
-    console.log(decode('**********'))
-  
-   /* function decode(expr) {
-      var Mas = expr.toString().split("");
-      var New_array = [];
-  
-      for (var i = 0; i <= Mas.length/10; i++) {
-        
-        New_array.push(Mas.splice(0,10).join(""));
-      }
-      return New_array;
-  }*/
+
+console.log(   check('[(])', [['(', ')'], ['[', ']']])  )
